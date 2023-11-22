a249df5e9914cdff1f1a9a8f215841f8e7dacca2
Mohamed Sameh <mosameh764@gmail.com>
2023-11-22 10:10:31 +0200

    Add project files.

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,50 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Polly;
+using Ocelot.Values;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+/**ASSIGN OCELOT.JSON FILE CONFIGURATIONS**/
+builder.Configuration.AddJsonFile("ocelot.json",optional: false,reloadOnChange: true);
+
+builder.Services.AddAuthentication("Bearer")
+	.AddJwtBearer(options =>
+	{
+		options.Authority = "https://localhost:7218";
+		options.TokenValidationParameters.ValidateAudience = false;
+	});
+
+/**CUSTOME CONFIGURATIONS FOR OCELOT CACHING**/
+builder.Services.AddOcelot(builder.Configuration)
+	.AddCacheManager(x =>
+	{
+		x.WithDictionaryHandle();
+	})
+	.AddPolly();
+
+var app = builder.Build();
+
+
+if(app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+/**USE OCELOT**/
+app.UseOcelot().Wait();
+
+app.Run();
---
+++ IdentityServer/Config.cs
@@ -0,0 +1,35 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Test;
+
+namespace IdentityServer
+{
+	public static class Config
+	{
+		public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>
+		{
+			new ApiScope("api1","MoviesAPI"),
+			new ApiScope("api2","SeriesAPI")
+		};
+
+		public static IEnumerable<Client> Clients => new List<Client>
+		{
+			new Client
+			{
+				ClientId = "client1",
+				ClientSecrets = {new Secret("secretKey".Sha256())},
+				AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+				AllowedScopes = {"api1","api2" }
+			}
+		};
+
+		public static List<TestUser> TestUsers => new List<TestUser>
+		{
+			new TestUser
+			{
+				SubjectId = "26c742ad-10e9-4727-b9eb-da166ef19086",
+				Username = "MohamedSameh",
+				Password = "Sameh1998",
+			}
+		};
+	}
+}
